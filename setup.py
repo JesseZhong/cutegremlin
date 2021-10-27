c93bd57d92ff86994e3e38386ea688127460036a
Jesse Zhong <jessetakuto@gmail.com>
2021-10-27 16:08:47 -0400

    Fixed stuff.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
    name='cutegremlin',
-   version='1.0.2',
+   version='1.0.3',
    description='A bunch of painfully dull helpers for AWS and others resources.',
    license='MIT',
    long_description=README,
